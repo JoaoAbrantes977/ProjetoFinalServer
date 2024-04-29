382891b5c14f48261699a30dab6f05b4f7410d56
João Abrantes <101903253+JoaoAbrantes977@users.noreply.github.com>
2024-04-29 01:15:01 +0100

    mysql database connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
 //const notasRoute = require("./Controllers/notas");
 const app = express();
@@ -9,6 +10,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+//CODE FOR CONNECTION TO DATABASE
+const dbase = mysql.createConnection({
+    host:"localhost",
+    port:"3306",
+    user:"root",
+    database:"projeto_final",
+    });
+    
+    dbase.connect(function(err){
+    if(err)throw err;
+    
+    console.log("Database Connected!");
+    
+    });
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
