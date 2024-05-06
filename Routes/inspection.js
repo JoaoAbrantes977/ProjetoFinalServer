8a873a8d8f454cd95e5c02aed21fb22ea594b797
João Abrantes <101903253+JoaoAbrantes977@users.noreply.github.com>
2024-05-06 03:13:14 +0100

    create inspection added

--- Routes/inspection.js
+++ Routes/inspection.js
@@ -10,13 +10,15 @@ const router = express.Router();
 router.post('/inspection', (req, res) => {
   //variavel global database
   const db = global.db;
-  const { descricao, tipologia, area, altura, data_util, 
+
+  const tipoValues = { descricao, tipologia, area, altura, data_util, 
   periodo, funcionamento, utilizacao, num_fachadas, num_coberturas,
   pre_esforco, km_inicio, km_fim, material_estrutural, extensao_tabuleiro,
   largura_tabuleiro, vias_circulacao, material_pavi, sistema_drenagem, num_pilares,
   geometria, material_revestimento, createdOn, updatedOn, id_pais } = req.body;
 
   const tipoQuery = 'INSERT INTO tipo SET ?';
+
   db.query(tipoQuery, tipoValues, (err, tipoResult) => {
     if (err) {
       console.error('Error inserting into tipo table: ' + err.stack);
@@ -28,15 +30,15 @@ router.post('/inspection', (req, res) => {
     const tipoId = tipoResult.insertId;
 
     // Now, create the inspecao record using the tipo ID
-    const inspecaoValues = {
+    const inspectionValues = {
       createdOn: new Date(),
       updatedOn: new Date(),
-      id_utilizador: req.body.id_utilizador, // Assuming id_utilizador is provided in the request body
+      id_utilizador: 1, // Estou a passar pelo body apenas para testar, depois guardo o id no shared_preferences do flutter
       id_tipo: tipoId
     };
 
     const inspecaoQuery = 'INSERT INTO inspecao SET ?';
-    db.query(inspecaoQuery, inspecaoValues, (err, inspecaoResult) => {
+    db.query(inspecaoQuery, inspectionValues, (err, inspecaoResult) => {
       if (err) {
         console.error('Error creating inspecao record: ' + err.stack);
         res.status(500).send('Error creating inspecao record');
