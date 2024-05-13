9a1984fc110ff7b88d5be733c4e05cde8d26d04b
João Abrantes <101903253+JoaoAbrantes977@users.noreply.github.com>
2024-05-13 01:39:44 +0100

    flight_plans finished

--- Routes/flight_plan.js
+++ Routes/flight_plan.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+// Route to handle GET requests for retrieving flight plan information by inspection ID
+router.get('/:id', (req, res) => {
+  //variavel global database
+  const db = global.db;
+  const inspectionId = req.params.id;
+
+  // SQL query to retrieve flight plan information by inspection ID
+  const query = `
+    SELECT p.*, i.id AS inspecao_id
+    FROM plano_voo p
+    INNER JOIN inspecao i ON p.id_inspecao = i.id
+    WHERE i.id = ?
+  `;
+
+  // Execute the SQL query with the inspection ID as a parameter
+  db.query(query, [inspectionId], (error, results, fields) => {
+    if (error) {
+      console.error('Error retrieving flight plans:', error);
+      res.status(500).send("Error retrieving flight plans");
+      return;
+    }
+    console.log('Flight plans retrieved successfully');
+    res.status(200).json(results); // Send the flight plan information as JSON response
+  });
+});
+
+
 // Route to handle POST requests for inserting flight plan information
 router.post('/create', (req, res) => {
       //variavel global database
@@ -25,6 +52,43 @@ router.post('/create', (req, res) => {
     });
   });
 
+// Route to handle PATCH requests for updating flight plan information by ID
+router.patch('/:id', (req, res) => {
+  //variavel global database
+  const db = global.db;
+  const flightPlanId = req.params.id;
+  
+  // Extracting flight plan data from request body
+  const flightPlanData = req.body;
+
+  //################################################
+  // o id da inspecção é depois passado no frontend
+  //################################################
+
+  // SQL query to update flight plan data by ID
+  const query = `
+    UPDATE plano_voo 
+    SET ?,
+    updatedOn = CURDATE()
+    WHERE id = ?
+  `;
+  
+  // Execute the SQL query with flight plan data and ID as parameters
+  db.query(query, [flightPlanData, flightPlanId], (error, results, fields) => {
+    if (error) {
+      console.error('Error updating flight plan:', error);
+      res.status(500).send("Error updating flight plan");
+      return;
+    }
+    if (results.affectedRows === 0) {
+      // If no rows were affected, it means no flight plan was found with the provided ID
+      res.status(404).send("Flight plan not found");
+      return;
+    }
+    console.log('Flight plan updated successfully');
+    res.status(200).send("Flight plan updated successfully");
+  });
+});
 
 
 
