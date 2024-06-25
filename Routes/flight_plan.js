c9a9139bd13fe50e29f6c6777bfacafb2b743a9e
João Abrantes <101903253+JoaoAbrantes977@users.noreply.github.com>
2024-06-25 03:19:04 +0100

    Update flight_plan.js

--- Routes/flight_plan.js
+++ Routes/flight_plan.js
@@ -57,27 +57,46 @@ router.get('/flightPlan/:id', (req, res) => {
 
 // Route to handle POST requests for inserting flight plan information
 router.post('/create', (req, res) => {
-      //variavel global database
-      const db = global.db;
-    
-    // Extracting flight plan data from request body
-    const flightPlanData = { distancia_objecto, osd, area_mapeamento, taxa_sobrepos, intervalo_foto, tipo_voo,
-        linha_voo, id_inspecao, id_drone} = req.body;
+  //variavel global database
+  const db = global.db;
 
-    // SQL query to insert flight plan data into the database
-    const query = 'INSERT INTO plano_voo SET ?, createdOn = CURDATE(), updatedOn = CURDATE()';
-  
-    // Execute the SQL query
-    db.query(query, flightPlanData, (error, results, fields) => {
-      if (error) {
-        console.error('Error inserting flight plan:', error);
-        res.status(500).send("Error inserting flight plan");
-        return;
-      }
-      console.log('Flight plan inserted successfully');
-      res.status(200).send("Flight plan inserted successfully");
+  // Extracting flight plan data from request body
+  const flightPlanData = {
+    distancia_objecto,
+    osd,
+    area_mapeamento,
+    taxa_sobrepos,
+    intervalo_foto,
+    tipo_voo,
+    linha_voo,
+    id_inspecao,
+    id_drone
+  } = req.body;
+
+  // SQL query to insert flight plan data into the database
+  const query = 'INSERT INTO plano_voo SET ?, createdOn = CURDATE(), updatedOn = CURDATE()';
+
+  // Execute the SQL query
+  db.query(query, flightPlanData, (error, results, fields) => {
+    if (error) {
+      console.error('Error inserting flight plan:', error);
+      res.status(500).send("Error inserting flight plan");
+      return;
+    }
+
+    // Extract the ID of the newly inserted row
+    const newPlanoVooId = results.insertId;
+
+    console.log('Flight plan inserted successfully with ID:', newPlanoVooId);
+
+    // Send the newPlanoVooId back in the response
+    res.status(200).json({
+      id: newPlanoVooId,
+      message: "Flight plan inserted successfully"
     });
   });
+});
+
 
 // Route to handle PATCH requests for updating flight plan information by ID
 router.patch('/:id', (req, res) => {
